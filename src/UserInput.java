f5cae83157312eb7e29b68546c8ef2cd053066a8
Mannarinoo <joey_mannarino@yahoo.com>
2020-12-03 10:41:36 -0500

    frequency > 30

--- src/UserInput.java
+++ src/UserInput.java
@@ -13,13 +13,18 @@ public class UserInput {
         System.out.println("How often would you like to check for deals? (min)");
         System.out.println("Please enter a number greater than 30");
         frequency = scanner.nextInt();
-
+        if (frequency < 30) {
+        	System.out.println("Please enter a number greater than 30");
+        	frequency = scanner.nextInt();
+        }else {
+        
         System.out.println("Item(s) Searched:");
         for (int i = 0; i < keyWord.length; i++) {
         	System.out.println(keyWord[i]);
         }
         System.out.println("Frequency of Search: " + frequency +" min");
         scanner.close();
+        }
     }
     //helper methods
     public int getFrequency() {
